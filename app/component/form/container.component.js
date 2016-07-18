9ab6cd8c4d237819320cd0e275a071d17804734b
Zack Yang <zack9433@gmail.com>
2016-07-18 10:56:42 +0800

    fix(component): require correct module

--- app/component/form/container.component.js
+++ app/component/form/container.component.js
@@ -1,6 +1,8 @@
+import CellularFormContainerController from './container.controller';
+
 const CellularFormContainerComponent = {
   template: `<sanji-cellular-form data="vm.data" on-submit="vm.onSave(data)"></sanji-cellular-form>`,
-  controller: 'CellularFormContainerController',
+  controller: CellularFormContainerController,
   controllerAs: 'vm'
 };
 export default CellularFormContainerComponent;
