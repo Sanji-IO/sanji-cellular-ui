abf8240a6d5945823d96166f06f31e73dd9cf341
Zack Yang <zack9433@gmail.com>
2016-09-22 14:17:56 +0800

    chore(build): modify for webpack 2 breaking changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,17 @@
-'use strict';
-
-var path = require('path');
-var webpack = require('webpack');
-var ProgressBarPlugin = require('progress-bar-webpack-plugin');
-var NODE_ENV = process.env.NODE_ENV;
-var BASE_PATH = process.env.BASE_PATH;
-var nodeRoot = path.join(__dirname, 'node_modules');
-var appRoot = path.join(__dirname, 'app');
-var config = {
+const path = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV;
+const BASE_PATH = process.env.BASE_PATH;
+const nodeRoot = path.join(__dirname, 'node_modules');
+const appRoot = path.join(__dirname, 'app');
+const config = {
   context: appRoot,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'sanji-cellular-ui.js'
   },
   resolve: {
-    root: [nodeRoot],
-    // npm-linked packages can locate missing dependencies in app's node_modules
-    fallback: nodeRoot,
     alias: {
       'angular-material.css': nodeRoot + '/angular-material/angular-material.css',
       'angular-material-icons.css': nodeRoot + '/angular-material-icons/angular-material-icons.css',
@@ -24,18 +19,15 @@ var config = {
       'angular-sanji-window.css': nodeRoot + '/angular-sanji-window/dist/angular-sanji-window.css',
       'toastr.css': nodeRoot + '/toastr/build/toastr.css'
     },
-    extensions: ['', '.js', '.json', 'html', 'scss', 'css']
+    extensions: ['.js', '.json', 'html', 'scss', 'css']
   },
   module: {
-    preLoaders: [
-      {test: /\.js$/, loader: 'eslint', exclude: /(node_modules)/}
-    ],
-    loaders: [
+    rules: [
+      { test: /\.js$/, loader: 'eslint', exclude: /node_modules/, enforce: 'pre' },
       { test: /\.js$/, loader: 'babel?cacheDirectory', exclude: /node_modules/ },
       { test: /\.json$/, loader: 'json', exclude: /node_modules/ },
       { test: /\.html$/, loader: 'ng-cache?prefix=[dir]/[dir]', exclude: [/node_modules/, path.join(__dirname, '/app/index.html')] }
-    ],
-    noParse: []
+    ]
   },
   plugins: [
     new ProgressBarPlugin(),
@@ -44,8 +36,7 @@ var config = {
       __DEV__: 'development' === NODE_ENV,
       __RELEASE__: 'production' === NODE_ENV,
       __BASE_PATH__: JSON.stringify(BASE_PATH) || '"http://localhost:8000"'
-    }),
-    new webpack.NoErrorsPlugin()
+    })
   ]
 };
 
