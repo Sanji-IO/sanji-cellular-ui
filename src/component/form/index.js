4222865647803a081ef913873ad6bca6a474e2f0
Zack Yang <zack9433@gmail.com>
2017-03-31 09:33:25 +0800

    feat(field): modify reboot label style

--- src/component/form/index.js
+++ src/component/form/index.js
@@ -1,11 +1,12 @@
 import angular from 'angular';
 
+import './form.style.scss';
 import './form.tpl.html';
 import CellularFormContainerComponent from './container.component';
 import CellularFormComponent from './form.component';
 
-const sjCellularForm = angular.module('sanji.cellular.form', [])
+const sjCellularForm = angular
+  .module('sanji.cellular.form', [])
   .component('sanjiCellularFormContainer', CellularFormContainerComponent)
-  .component('sanjiCellularForm', CellularFormComponent)
-  .name;
-export {sjCellularForm};
+  .component('sanjiCellularForm', CellularFormComponent).name;
+export { sjCellularForm };
